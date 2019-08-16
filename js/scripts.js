1bb884a32a4555dbcf20bf7a0dcd6694bcb53a6b
AndyOmondi17 <andyomondi17@gmail.com>
2019-08-16 12:16:05 +0300

    add function to alert information keyed in the form.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function testResults (form) {
+    var firstName = form.firstName.value;
+    var surName= form.surName.value;
+    var birthDate= form.birthDate.value;
+    return alert ("Hi "+firstName+" "+surName+".Your birthday is "+birthDate);
+}
