22ed42b1782d595d656452dff0c5f458d9a649a0
AndyOmondi17 <andyomondi17@gmail.com>
2019-08-16 17:18:05 +0300

    add functionality to the radiobutton

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
-function testResults (form) {
-    var firstName = form.firstName.value;
-    var surName= form.surName.value;
-    var gender=form.gender.value;
-    var birthDate= form.birthDate.value;
-    return alert ("Hi "+firstName+" "+surName+".Your birthday is "+birthDate);
+var gd;
+var firstName=document.getElementByName('firstName');
+var gender=document.getElementsByName('gender');
+var birthDate=document.getElementByName('birthDate');
+var surName=document.getElementByName('surname');
+
+function getUSerData (form){
+    firstName = form.firstName.value;
+    surName= form.surName.value;
+    gender=document.getElementsByName('gender');
+    birthDate= form.birthDate.value;
+
+    if(gender[0].checked){
+      gd=gender[0].value;
+
+    }
+    else {
+       gd=gender[1].value;
+    }
+
+
+    return alert ("Hi "+firstName+" "+surName+".Your birthday is "+birthDate+"Your gender is "+gd);
+}
+function checkFormInput(){
+  if
 }
