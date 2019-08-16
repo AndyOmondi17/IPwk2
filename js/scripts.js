5ef7e68ff7b29226cf346e1113d08c02d58a81fa
AndyOmondi17 <andyomondi17@gmail.com>
2019-08-16 12:31:06 +0300

    add checkbox instead of radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function testResults (form) {
     var firstName = form.firstName.value;
     var surName= form.surName.value;
+    var gender=form.gender.value;
     var birthDate= form.birthDate.value;
     return alert ("Hi "+firstName+" "+surName+".Your birthday is "+birthDate);
 }
